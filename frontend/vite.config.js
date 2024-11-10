0b7657578fd3d47b11ad1ccdef5d0c90df4b4e0a
Arpit kumar Kanwar <118201864+arpitkumarfault@users.noreply.github.com>
2024-11-10 11:41:07 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://task-management-new-backend.onrender.com',
+      '/api': 'https://task-hub-a4wn.onrender.com',
     }
   },
   plugins: [react()],
