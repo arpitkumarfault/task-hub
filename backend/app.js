540d47952349d3f14d18e41f36e557d348dd4958
kanwararpit <arpit.kanwar@quarkifi.com>
2024-11-12 10:40:27 +0530

    add new change

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,9 @@ app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+app.get('/ping',(req,res)=>{
+    res.send("PONG")
+})
 // API routes
 app.use('/api/v4/user', router)
 
